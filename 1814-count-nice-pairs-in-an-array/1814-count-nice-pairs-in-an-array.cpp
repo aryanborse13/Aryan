75d6b43e423ffc4a299ee6f1cedd512129366b7f
Aryan Borse <aryanborse13@gmail.com>
2026-07-29 15:36:09 +0530

    Time: 27 ms (29.06%), Space: 60.9 MB (60.4%) - LeetHub

---
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    int rev(int n){
+        int r = 0;
+        while(n>0){
+            r *= 10;
+            r += (n%10);
+            n /= 10;
+        }
+        return r;
+    }
+    int fact(int n){
+        int f = 1;
+        for(int i =2;i<=n;i++){
+            f *= i;
+        }
+        return f;
+    }
+
+    int combi(int n, int r){
+        return fact(n)/fact(r)*fact(n-r);
+    }
+    int countNicePairs(vector<int>& nums) {
+        int n = nums.size();
+        long long count =0;
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++){
+            nums[i] -= rev(nums[i]);
+        }
+     for(int i=0; i<n; i++){
+        if(m.find(nums[i]) != m.end()){
+            count += m[nums[i]];
+            m[nums[i]]++;
+        }else{
+            m[nums[i]]++;
+        }
+     }
+        return count%1000000007;
+    }
+};
